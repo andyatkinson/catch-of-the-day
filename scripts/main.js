369d9e79cf0d1be61d37f84a829f2960742870a4
Andy Atkinson <andyatkinson@gmail.com>
2016-02-24 15:19:21 -0600

    Initial commit

---
+++ scripts/main.js
@@ -0,0 +1,24 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+/* StorePicker
+*/
+
+var StorePicker = React.createClass({
+
+  render: function() {
+    var name = 'andy';
+
+    return (
+      <form className='store-selector'>
+        <h2>Please enter a store {name}</h2>
+        <input type='text' ref='storeId' required />
+        <input type='Submit' />
+      </form>
+    )
+  }
+
+});
+
+
+ReactDOM.render(<StorePicker/>, document.querySelector('#main'));
