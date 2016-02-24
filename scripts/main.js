156594ab55d351b1c255d0cc764d430bca6f3eb2
Andy Atkinson <andyatkinson@gmail.com>
2016-02-24 15:29:41 -0600

    Progress from video #5

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,61 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+/*
+* App
+*/
+
+var App = React.createClass({
+
+  render : function() {
+    return (
+      <div className='catch-of-the-day'>
+        <div className='menu'>
+          <Header/>
+        </div>
+        <Order/>
+        <Inventory/>
+      </div>
+    )
+  }
+});
+
+/*
+* Header
+*/
+
+var Header = React.createClass({
+  render : function() {
+    return (
+      <p>Header</p>
+    )
+  }
+});
+
+/*
+* Order
+*/
+
+var Order = React.createClass({
+  render : function() {
+    return (
+      <p>Order</p>
+    )
+  }
+});
+
+/*
+* Inventory
+*/
+
+var Inventory = React.createClass({
+  render : function() {
+    return (
+      <p>Inventory</p>
+    )
+  }
+});
+
 /* StorePicker
 */
 
@@ -21,4 +76,4 @@ var StorePicker = React.createClass({
 });
 
 
-ReactDOM.render(<StorePicker/>, document.querySelector('#main'));
+ReactDOM.render(<App/>, document.querySelector('#main'));
