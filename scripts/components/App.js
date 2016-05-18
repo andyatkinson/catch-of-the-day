4bf61c6892bf4631816fe3859c0fdf9466ff5e87
Andy Atkinson <andyatkinson@gmail.com>
2016-05-18 14:54:20 -0500

    Ep 27 - convert App component

--- scripts/components/App.js
+++ scripts/components/App.js
@@ -1,32 +1,33 @@
+/*
+* App
+*/
+
 import React from 'react';
 import Header from './Header';
 import Fish from './Fish';
 import Order from './Order';
 import Inventory from './Inventory';
 import StorePicker from './StorePicker';
+import Catalyst from 'react-catalyst';
+import reactMixin from 'react-mixin';
+import autobind from 'autobind-decorator';
 
 //firebase
 import Rebase from 're-base';
 var base = Rebase.createClass('https://boiling-torch-6140.firebaseio.com/');
 
-import Catalyst from 'react-catalyst';
+@autobind
+class App extends React.Component {
 
-
-/*
-* App
-*/
-
-var App = React.createClass({
-  mixins : [Catalyst.LinkedStateMixin],
-
-  // part of the react lifecycle
-  getInitialState : function() {
-    return {
+  constructor() {
+    super();
+    this.state = {
       fishes: {},
       order: {}
-    };
-  },
-  componentDidMount : function() {
+    }
+  }
+
+  componentDidMount() {
     // load from firebase
     base.syncState(this.props.params.storeId + '/fishes', {
       context: this,
@@ -40,40 +41,47 @@ var App = React.createClass({
         order : JSON.parse(localStorageRef)
       });
     }
-  },
-  componentWillUpdate : function(nextProps, nextState) {
+  }
+
+  componentWillUpdate(nextProps, nextState) {
     //save it to local storage, key/value pair, key is fish ID and value is the fish
     localStorage.setItem('order-' + this.props.params.storeId, JSON.stringify(nextState.order));
-  },
-  addToOrder : function(key) {
+  }
+
+  addToOrder(key) {
     this.state.order[key] = this.state.order[key] + 1 || 1;
     this.setState({ order: this.state.order });
-  },
-  addFish: function(fish) {
+  }
+
+  addFish(fish) {
     var timestamp = (new Date()).getTime();
     // update the state object
     this.state.fishes['fish-' + timestamp] = fish;
     // set the state, this.state is entire state, but we should pass just the changes
     this.setState({ fishes: this.state.fishes });
-  },
-  removeFromOrder : function(key) {
+  }
+
+  removeFromOrder(key) {
     delete this.state.order[key];
     this.setState({ order: this.state.order });
-  },
-  removeFish: function(key) {
+  }
+
+  removeFish(key) {
     if(confirm('Are you sure you want to remove this fish?')) {
       this.state.fishes[key] = null; //deletes the item
       this.setState({fishes: this.state.fishes});
     }
-  },
-  loadSamples: function() {
-    this.setState({ fishes: require('../sample-fishes.js') });
-  },
-  renderFish: function(key) {
-    return <Fish key={key} index={key} details={this.state.fishes[key]} addToOrder={this.addToOrder} />;
-  },
+  }
 
-  render : function() {
+  loadSamples() {
+    this.setState({ fishes: require('../sample-fishes.js') });
+  }
+
+  renderFish(key) {
+    return <Fish key={key} index={key} details={this.state.fishes[key]} addToOrder={this.addToOrder} />;
+  }
+
+  render() {
     return (
       <div className='catch-of-the-day'>
         <div className='menu'>
@@ -85,11 +93,13 @@ var App = React.createClass({
         <Order fishes={this.state.fishes} order={this.state.order} removeFromOrder={this.removeFromOrder} />
         <Inventory addFish={this.addFish} loadSamples={this.loadSamples}
           fishes={this.state.fishes}
-          linkState={this.linkState}
+          linkState={this.linkState.bind(this)}
           removeFish={this.removeFish}/>
       </div>
-    );
+    )
   }
-});
+}
+
+reactMixin.onClass(App, Catalyst.LinkedStateMixin);
 
 export default App;
